86147aeb72bfe5b4934b180ae71001ab5a43cd36
MyLanNguyen999 <mylannguyen192@gmail.com>
2024-04-16 08:49:25 -0400

    function remove all records

--- delete-all.php
+++ delete-all.php
@@ -8,18 +8,18 @@
 <?php
 require 'include/auth.php';
 
-$gameId = $_GET['gameId'];
+// $gameId = $_GET['gameId'];
 
-if (is_numeric($gameId)) {
+// if (is_numeric($gameId)) {
     require 'include/db.php';
 
-    $sql = "DELETE FROM examgames WHERE gameId = :gameId";
+    $sql = "DELETE FROM examgames";
     $cmd = $db->prepare($sql);
-    $cmd->bindParam(':gameId', $gameId, PDO::PARAM_INT);
+    // $cmd->bindParam(':gameId', $gameId, PDO::PARAM_INT);
     $cmd->execute();
 
     $db = null;
-}
+// }
 
 header('location:games.php');
 ?>
