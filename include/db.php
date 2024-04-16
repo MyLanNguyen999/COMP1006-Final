4cf1acc58282bbd04088f6ff44cdfc3c16ca1712
MyLanNguyen999 <mylannguyen192@gmail.com>
2024-04-16 08:09:10 -0400

    add credentials to db.php

--- include/db.php
+++ include/db.php
@@ -1,3 +1,3 @@
 <?php
-$db = new PDO('mysql:host=172.31.22.43;dbname=', '', '');
+$db = new PDO('mysql:host=172.31.22.43;dbname=MyLan200567763', 'MyLan200567763', 'D4DH4hX6-O');
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
